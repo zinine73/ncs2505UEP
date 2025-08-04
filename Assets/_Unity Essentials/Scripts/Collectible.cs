04b3ebfaf0fd0b4a6f7918acc4065fa35fa1aa51
zinine73 <zinine@naver.com>
2025-08-04 15:52:52 +0900

    isTrigger

--- Assets/_Unity Essentials/Scripts/Collectible.cs
+++ Assets/_Unity Essentials/Scripts/Collectible.cs
@@ -5,13 +5,27 @@ using UnityEngine;
 public class Collectible : MonoBehaviour
 {
     public float rotationSpeed;
+    public GameObject onCollectEffect;
     void Start()
     {
-        
+
     }
 
     void Update()
     {
         transform.Rotate(0, rotationSpeed, 0);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            // Destroy collectible
+            Destroy(gameObject);
+            // Instantiate particle effect
+            Instantiate(onCollectEffect,
+                transform.position,
+                transform.rotation);
+        }
     }
 }
